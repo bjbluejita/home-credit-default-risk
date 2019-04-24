7a5707a943a1ba114f03a185024ce3465209e3da
LY <30115682@qq.com>
2019-04-24 17:55:21 +0800

    add features

--- feature - application.py
+++ feature - application.py
@@ -10,6 +10,7 @@
 import pandas as pd
 import os
 import numpy as np
+import cmath
 import matplotlib.pyplot as plt
 import seaborn as sns
 #%matplotlib inline
@@ -186,26 +187,91 @@ application_test['DAYS_BIRTH_YEAR'] = pd.cut( np.abs( np.round( application_test
 application_train['DAYS_BIRTH_log'] = np.log( application_train.DAYS_BIRTH )
 application_test['DAYS_BIRTH_log'] = np.log( application_test.DAYS_BIRTH )
 
-def income_type_to_level( income_type ):
-    if income_type == 'Student' or income_type == 'Businessman': return 0
-    elif income_type == 'Pensioner': return 1
-    elif income_type == 'State servant': return 2
-    elif income_type == 'Commercial associate': return 3
-    elif income_type == 'Working': return 4
-    elif income_type == 'Unemployed': return 5
-    elif income_type == 'Maternity leave': return 6
-    else: return 0
-
 application_train['log_TERM'] = np.log( application_train.TERM )
 application_train['RATIO_ANNUITY_INCOME'] =  application_train.AMT_ANNUITY / application_train.AMT_INCOME_TOTAL
 application_train['NAME_TYPE_SUITE_LEVEL'] = application_train['NAME_TYPE_SUITE'].map(lambda x:0 if x=='Unaccompanied' else 1 )
-application_train['NAME_INCOME_TYPE_LEVEL'] = application_train.NAME_INCOME_TYPE.map( lambda x: income_type_to_level(x) )
+application_train['NAME_INCOME_TYPE_LEVEL'] = application_train.NAME_INCOME_TYPE.map( {'Student': 0, 'Businessman':0,
+                                                                                       'Pensioner': 1,
+                                                                                       'State servant': 2,
+                                                                                       'Commercial associate': 3,
+                                                                                       'Working': 4,
+                                                                                       'Unemployed': 5,
+                                                                                       'Maternity leave': 6} )
 
 
 application_test['log_TERM'] = np.log( application_test.TERM )
 application_test['RATIO_ANNUITY_INCOME'] =  application_test.AMT_ANNUITY / application_test.AMT_INCOME_TOTAL
 application_test['NAME_TYPE_SUITE_LEVEL'] = application_test['NAME_TYPE_SUITE'].map(lambda x:0 if x=='Unaccompanied' else 1 )
-application_test['NAME_INCOME_TYPE_LEVEL'] = application_test.NAME_INCOME_TYPE.map( lambda x: income_type_to_level(x) )
+application_test['NAME_INCOME_TYPE_LEVEL'] = application_test.NAME_INCOME_TYPE.map( {'Student': 0, 'Businessman':0,
+                                                                                     'Pensioner': 1,
+                                                                                     'State servant': 2,
+                                                                                     'Commercial associate': 3,
+                                                                                     'Working': 4,
+                                                                                     'Unemployed': 5,
+                                                                                     'Maternity leave': 6} )
+
+#from URL:https://github.com/neptune-ml/open-solution-home-credit/blob/master/src/feature_extraction.py
+application_train['annuity_income_percentage'] = application_train.AMT_ANNUITY / application_train.AMT_INCOME_TOTAL
+application_train['cat_to_birth_ratio'] = application_train.OWN_CAR_AGE / application_train.DAYS_BIRTH
+application_train['car_to_employ_ratio'] = application_train.OWN_CAR_AGE / application_train.DAYS_EMPLOYED
+application_train['children_ratio'] = application_train.CNT_CHILDREN / application_train.CNT_FAM_MEMBERS
+application_train['credit_to_annuity_ratio'] = application_train.AMT_CREDIT / application_train.AMT_ANNUITY
+application_train['credit_to_goods_ratio'] = application_train.AMT_CREDIT / application_train.AMT_GOODS_PRICE
+application_train['credit_to_income_ratio'] = application_train.AMT_CREDIT / application_train.AMT_INCOME_TOTAL
+application_train['days_employed_percentage'] = application_train.AMT_INCOME_TOTAL / application_train.AMT_CREDIT
+application_train['income_per_child'] = application_train.AMT_INCOME_TOTAL / ( 1 + application_train.CNT_CHILDREN )
+application_train['income_per_person'] = application_train.AMT_INCOME_TOTAL / application_train.CNT_FAM_MEMBERS
+application_train['payment_rate'] = application_train.AMT_ANNUITY / application_train.AMT_CREDIT
+application_train['phone_to_birth_ratio'] = application_train.DAYS_LAST_PHONE_CHANGE / application_train.DAYS_BIRTH
+application_train['phone_to_employ_ratio'] = application_train.DAYS_LAST_PHONE_CHANGE / application_train.DAYS_EMPLOYED
+application_train['external_sources_weighted'] = np.nansum(
+    np.asarray( [ 1.9, 2.1, 2.6] ) * application_train[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']], axis=1
+)
+application_train['cnt_non_child'] = application_train.CNT_FAM_MEMBERS - application_train.CNT_CHILDREN
+application_train['child_to_non_child_ratio'] = application_train.CNT_CHILDREN / application_train.cnt_non_child
+application_train['income_per_non_child'] = application_train.AMT_INCOME_TOTAL / application_train.cnt_non_child
+application_train['credit_per_person'] = application_train.AMT_CREDIT / application_train.CNT_FAM_MEMBERS
+application_train['credit_per_child'] = application_train.AMT_CREDIT / ( 1 + application_train.CNT_CHILDREN )
+application_train['credit_per_non_child'] = application_train.AMT_CREDIT / application_train.cnt_non_child
+application_train['ext_source_1_plus_2'] = np.nansum( application_train[['EXT_SOURCE_1', 'EXT_SOURCE_2']], axis=1 )
+application_train['ext_source_1_plus_3'] = np.nansum( application_train[['EXT_SOURCE_1', 'EXT_SOURCE_3']], axis=1 )
+application_train['ext_source_2_plus_3'] = np.nansum( application_train[['EXT_SOURCE_2', 'EXT_SOURCE_3']], axis=1 )
+application_train['ext_source_1_is_nan'] = np.isnan( application_train.EXT_SOURCE_1 ).map( {False:1, True:0} )
+application_train['ext_source_2_is_nan'] = np.isnan( application_train.EXT_SOURCE_2 ).map( {False:1, True:0} )
+application_train['ext_source_3_is_nan'] = np.isnan( application_train.EXT_SOURCE_3 ).map( {False:1, True:0} )
+application_train['hour_appr_process_start_radial_x'] = application_train.HOUR_APPR_PROCESS_START.apply(
+    lambda x: cmath.rect( 1, 2 * cmath.pi * x / 24 ).real
+)
+application_train['hour_appr_process_start_radial_y'] = application_train.HOUR_APPR_PROCESS_START.apply(
+    lambda x: cmath.rect( 1, 2 * cmath.pi * x / 24 ).imag
+)
+application_train['id_renewal_days'] = application_train.DAYS_ID_PUBLISH - application_train.DAYS_BIRTH
+application_train['id_renewal_years'] = ( application_train.DAYS_ID_PUBLISH - application_train.DAYS_BIRTH ) / 365
+application_train['id_renewal_days_issue'] = np.vectorize(
+    lambda x: max( list( set( [ min( x, age) for age in [0, 20*365, 25*365, 45*365]]) - set([x]))))\
+( application_train.id_renewal_days )
+application_train['id_renewal_years_issue'] = np.vectorize(
+    lambda x: max( list( set( [ min( x, age) for age in [0, 20, 25, 46]]) - set([x])))) \
+    ( application_train.id_renewal_years )
+application_train.loc[ application_train['id_renewal_days_issue'] <= 20*366, 'id_renewal_days_delay' ] = -1
+application_train.loc[ application_train['id_renewal_years_issue'] <= 20*366, 'id_renewal_years_delay' ] = -1
+application_train.loc[ application_train['id_renewal_days_issue'] > 20*366, 'id_renewal_days_delay' ] = \
+    application_train.loc[ application_train['id_renewal_days_issue'] > 20*366, 'id_renewal_days' ].values - \
+    application_train.loc[ application_train['id_renewal_days_issue'] > 20*366, 'id_renewal_days_issue' ]
+application_train.loc[ application_train['id_renewal_years_issue']>20, 'id_renewal_years_delay' ] = \
+    application_train.loc[ application_train['id_renewal_years_issue']>20, 'id_renewal_years'].values - \
+    application_train.loc[ application_train['id_renewal_years_issue']>20, 'id_renewal_years_issue' ]
+
+for function_name in ['min', 'max', 'sum', 'mean', 'nanmedian']:
+    application_train[  'external_source_{}'.format( function_name ) ] = eval( 'np.{}'.format( function_name ))(
+        application_train[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']], axis=1
+        )
+
+application_train['short_employment'] = ( application_train.DAYS_EMPLOYED < -2000 ).astype( int )
+application_train['young_age'] = ( application_train.DAYS_BIRTH < -14000 ).astype( int )
+
+
+
 
 #####  convert categorical variables to numericals
 from sklearn.preprocessing import LabelEncoder
